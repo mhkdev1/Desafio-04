e4c7537d8e790274f558c56c44b0b112e150b2b7
Matheus Kovalski <mhkovalsky@edu.univali.br>
2022-07-25 15:57:03 -0300

    CM- Criptografia de senhas

--- imobiliaria/src/main/java/br/com/saks/criptografia/Criptografia.java
+++ imobiliaria/src/main/java/br/com/saks/criptografia/Criptografia.java
@@ -3,11 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.saks.criptografia;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-/**
- *
- * @author mhkov
- */
 public class Criptografia {
-    
+    public static String criptografar(String senha) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest criptografia = MessageDigest.getInstance("SHA-256");
+        byte senhaCriptografada[] = criptografia.digest(senha.getBytes("UTF-8"));
+        String senhaEmString = new String(senhaCriptografada, "UTF-8");
+
+        return senhaEmString;
+    }
 }
