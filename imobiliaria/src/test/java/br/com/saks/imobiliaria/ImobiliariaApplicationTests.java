13a4e84a9c78d7dba91b65e7f366aedf2a8b68c4
Luiz_Tomasi <7915772@U702624.univali.br>
2022-07-25 16:44:37 -0300

    CM - Criado os testes unitarios do Cliente e Imoveis

--- imobiliaria/src/test/java/br/com/saks/imobiliaria/ImobiliariaApplicationTests.java
+++ imobiliaria/src/test/java/br/com/saks/imobiliaria/ImobiliariaApplicationTests.java
@@ -2,7 +2,9 @@ package br.com.saks.imobiliaria;
 
 
 import br.com.saks.imobiliaria.model.Cliente;
+import br.com.saks.imobiliaria.model.Imovel;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Date;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -40,5 +42,74 @@ class ImobiliariaApplicationTests {
                 .content(objectMapper.writeValueAsString(cliente)))
                 .andExpect(status().isOk());
     }
+    
+    @Test
+    void getCliente() throws Exception {
+       mockMvc.perform(MockMvcRequestBuilders.get("/clientes")
+       .contentType(MediaType.APPLICATION_JSON))
+       .andExpect(status().isOk());
+   } 
+    
+    @Test
+    void alteraCliente() throws Exception {
+       Cliente cliente = new Cliente();
+       cliente.setNome("Testa");
+       cliente.setSenha("Testa");
+       cliente.setEmail("Testa@Testa");
+       cliente.setTelefone("Testa");
+       mockMvc.perform(MockMvcRequestBuilders.put("/clientes/{id}",2)
+       .contentType(MediaType.APPLICATION_JSON)
+       .content(objectMapper.writeValueAsString(cliente)))
+       .andExpect(status().isOk());
+   }
+    
+    /*@Test
+    void deletaCliente() throws Exception {
+         mockMvc.perform(MockMvcRequestBuilders.delete("/clientes/{id}",1)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }*/
 
+    @Test
+    void createImovel() throws Exception {
+        Imovel imovel = new Imovel();
+        imovel.setTitulo("Teste");
+        imovel.setDescricao("Teste");
+        imovel.setDataCriacao(new Date());
+        imovel.setValor(2020);
+        imovel.setStatus(0);
+        mockMvc.perform(MockMvcRequestBuilders.post("/imoveis")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(imovel)))
+                .andExpect(status().isOk());
+    }
+
+   @Test
+   void getImovel() throws Exception {
+       mockMvc.perform(MockMvcRequestBuilders.get("/imoveis")
+       .contentType(MediaType.APPLICATION_JSON))
+
+       .andExpect(status().isOk());
+   } 
+    
+   @Test
+   void alteraImovel() throws Exception {
+       Imovel imovel = new Imovel();
+       imovel.setTitulo("Testa");
+       imovel.setDataCriacao(new Date());
+       imovel.setDescricao("Testa");
+       imovel.setValor(3030);
+       imovel.setStatus(1);
+       mockMvc.perform(MockMvcRequestBuilders.put("/imoveis/{id}",2)
+       .contentType(MediaType.APPLICATION_JSON)
+       .content(objectMapper.writeValueAsString(imovel)))
+       .andExpect(status().isOk());
+   }
+    
+    /*@Test
+    void deletaImovel() throws Exception {
+         mockMvc.perform(MockMvcRequestBuilders.delete("/imoveis/{id}",1)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }*/
 }
